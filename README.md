d2e6b4b4107948b4c1d00d2f0e6676253f81220a
Benjamin Symons <benjaminsymons@hotmail.com>
2019-12-06 14:58:41 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# User authentication using GraphQL and Passport.js
+
+> Tutorial followed to produce this repository
+> https://jkettmann.com/authentication-and-authorization-with-graphql-and-passport/
+
+### Follow up with
+
+> https://dev.to/jkettmann/password-based-authentication-with-graphql-and-passport-8nd
---
+++ api/index.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import session from 'express-session';
+import uuid from 'uuid/v4';
+import passport from 'passport';
+import User from './User';
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from './typeDefs';
+import resolvers from './resolvers';
+
+const PORT = 4000;
+
+const SESSION_SECRET = 'bad secret';
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+    const user = User.getUsers();
+    const matchingUser = users.find(user => user.id === id);
+    done(null, matchingUser);
+})
+
+const app = express();
+
+app.use(session({
+    genid: (req) => uuid(),
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({
+        user: req.user,
+        logout: () => req.logout(),
+    }),
+})
+
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}`);
+});
